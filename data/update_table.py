8d0db122f2280e18d57565a5ace2a9d29b742e91
Yashin Nikolay <kol.yashin@yandex.ru>
2023-04-07 15:19:30 +0300

    requirements

--- data/update_table.py
+++ data/update_table.py
@@ -8,6 +8,6 @@ def update(db_name):
     with db:
         sql.execute(f'UPDATE words SET winrate = CASE WHEN total=0 THEN 0 ELSE'
                     f' CAST(successful AS FLOAT) / CAST(total AS FLOAT) END')
-        sql.execute(f'UPDATE words SET coef = winrate*winrate*(1 - TANH({C}*CAST(julianday("now") - julianday(date) AS INT)))')
+        sql.execute(f'UPDATE words SET coef = SQRT(winrate)*(1 - TANH({C}*CAST(julianday("now") - julianday(date) AS INT)))')
     sql.close()
     db.close()
