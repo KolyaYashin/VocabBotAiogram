4d0ce5daf7628c34003b924f0345f4a40e186e01
Yashin Nikolay <kol.yashin@yandex.ru>
2023-04-12 11:35:35 +0300

    tanh

--- data/update_table.py
+++ data/update_table.py
@@ -6,6 +6,7 @@ C = 0.1
 def update(db_name):
     db = sqlite3.connect(db_name)
     db.create_function('sqrt', 1, math.sqrt)
+    db.create_function('tanh', 1, math.tanh)
     sql = db.cursor()
     with db:
         sql.execute(f'UPDATE words SET winrate = CASE WHEN total=0 THEN 0 ELSE'
